768a2844b429e6a565be59af5dc85fcc63d6ee16
ASHUTOSH57211 <45146282+ASHUTOSH57211@users.noreply.github.com>
2020-10-01 16:15:13 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -3,11 +3,14 @@ def is_leap(year):
     if (year % 4)==0:
         if (year % 100)!=0:
             leap = True
+            print("leap yr aita")
         else:
             if(year % 400)==0:
                 leap = True
+                print("eita b leap year")
             else:
                 leap = False
+                print("eita leap year nuha")
     return leap
 
 
