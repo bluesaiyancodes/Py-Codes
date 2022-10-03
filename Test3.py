481499a0cec1921a1c5e0542198bee6fb39671f2
Bishal Blue <43134322+bluesaiyancodes@users.noreply.github.com>
2022-10-03 15:39:31 +0900

    Update Test3.py
    Added Code for image cropping

--- Test3.py
+++ Test3.py
@@ -10,3 +10,13 @@ if __name__ == '__main__':
 
     print(b)
     print(type(b))
+    
+    def cropImage(self, img):
+        grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        _,thresholded = cv2.threshold(grayscale, 0, 255, cv2.THRESH_OTSU)
+        #cv2.imwrite("otsu.png", thresholded)
+        bbox = cv2.boundingRect(thresholded)
+        x, y, w, h = bbox
+        #print(bbox)
+        croppedImg = img[y:y+h, x:x+w]
+        return croppedImg
